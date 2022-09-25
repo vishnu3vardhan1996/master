9806c4dd6145525f0175bf7e90a8a6de811208e8
Vishnuvardhan Chellamuthu <107179629+vishnu03vardhan1996@users.noreply.github.com>
2022-09-26 01:54:07 +0530

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #rm /etc/nginx/conf.d/default.conf
-cp /var/www/html/firstnginx.location /etc/nginx/conf.d/
+#cp /var/www/html/firstnginx.location /etc/nginx/conf.d/
 #cp /var/www/html/default.conf /etc/nginx/conf.d/
+mkdir Firstfile
+chmod -R 777 Firstfile/
+cp -r var/www/html/* /usr/share/nginx/html/Firstfile
