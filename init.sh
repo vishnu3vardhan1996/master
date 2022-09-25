992040e7ea292dd99af3284135d2ed6232a6b375
Vishnuvardhan Chellamuthu <107179629+vishnu03vardhan1996@users.noreply.github.com>
2022-09-26 01:13:36 +0530

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #rm /etc/nginx/conf.d/default.conf
 cp /var/www/html/firstnginx.location /etc/nginx/conf.d/
-cp /var/www/html/default.conf /etc/nginx/conf.d/
+#cp /var/www/html/default.conf /etc/nginx/conf.d/
